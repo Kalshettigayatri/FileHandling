5d501c24084121c04686da913cd5ca0ac43e3c29
kalshettigayatri <gayatrikalshetti3@gmail.com>
2023-08-13 14:20:58 +0530

    Welcome to UC2

--- Program.cs
+++ Program.cs
@@ -1,90 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace AddressBookApp
+class AddressBookMain
 {
-    class Contact
+    static void Main(string[] args)
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Zip { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-    }
+        Console.WriteLine("Welcome to Address Book Program in C#");
 
-    class AddressBook
-    {
-        private List<Contact> contacts = new List<Contact>();
+        AddressBook addressBook = new AddressBook();
 
-        public void AddContact(Contact contact)
+        bool isRunning = true;
+        while (isRunning)
         {
-            contacts.Add(contact);
-        }
+            Console.WriteLine("\nSelect an option:");
+            Console.WriteLine("1. Add new contact");
+            Console.WriteLine("2. Display all contacts");
+            Console.WriteLine("3. Exit");
 
-        public void DisplayContacts()
-        {
-            Console.WriteLine("Contacts in Address Book:");
-            foreach (var contact in contacts)
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
             {
-                Console.WriteLine($"Name: {contact.FirstName} {contact.LastName}");
-                Console.WriteLine($"Address: {contact.Address}, {contact.City}, {contact.State}, {contact.Zip}");
-                Console.WriteLine($"Phone: {contact.PhoneNumber}");
-                Console.WriteLine($"Email: {contact.Email}");
-                Console.WriteLine();
+                case 1:
+                    addressBook.AddContact();
+                    break;
+                case 2:
+                    addressBook.DisplayContacts();
+                    break;
+                case 3:
+                    isRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please select a valid option.");
+                    break;
             }
         }
+
+        Console.WriteLine("Address Book program has ended.");
+    }
+}
+
+class AddressBook
+{
+    private List<Contact> contacts;
+
+    public AddressBook()
+    {
+        contacts = new List<Contact>();
     }
 
-    class AddressBookMain
+    public void AddContact()
     {
-        static void Main(string[] args)
+        Console.WriteLine("Enter name:");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Enter phone number:");
+        string phoneNumber = Console.ReadLine();
+
+        Console.WriteLine("Enter email:");
+        string email = Console.ReadLine();
+
+        Console.WriteLine("Enter address:");
+        string addressInfo = Console.ReadLine();
+
+        Contact newContact = new Contact(name, phoneNumber, email, addressInfo);
+        contacts.Add(newContact);
+
+        Console.WriteLine("Contact added successfully!");
+    }
+
+    public void DisplayContacts()
+    {
+        if (contacts.Count == 0)
         {
-            AddressBook addressBook = new AddressBook();
-
-            Console.WriteLine("Welcome to Address Book");
-            Console.Write("Enter First Name: ");
-            string firstName = Console.ReadLine();
-
-            Console.Write("Enter Last Name: ");
-            string lastName = Console.ReadLine();
-
-            Console.Write("Enter Address: ");
-            string address = Console.ReadLine();
-
-            Console.Write("Enter City: ");
-            string city = Console.ReadLine();
-
-            Console.Write("Enter State: ");
-            string state = Console.ReadLine();
-
-            Console.Write("Enter Zip: ");
-            string zip = Console.ReadLine();
-
-            Console.Write("Enter Phone Number: ");
-            string phoneNumber = Console.ReadLine();
-
-            Console.Write("Enter Email: ");
-            string email = Console.ReadLine();
-
-            Contact newContact = new Contact
-            {
-                FirstName = firstName,
-                LastName = lastName,
-                Address = address,
-                City = city,
-                State = state,
-                Zip = zip,
-                PhoneNumber = phoneNumber,
-                Email = email
-            };
-
-            addressBook.AddContact(newContact);
-            Console.WriteLine("Contact added successfully.");
-
-            addressBook.DisplayContacts();
+            Console.WriteLine("No contacts found.");
+            return;
         }
+
+        Console.WriteLine("List of contacts:");
+        foreach (var contact in contacts)
+        {
+            contact.Display();
+        }
+    }
+}
+
+class Contact
+{
+    public string Name { get; private set; }
+    public string PhoneNumber { get; private set; }
+    public string Email { get; private set; }
+    public string AddressInfo { get; private set; }
+
+    public Contact(string name, string phoneNumber, string email, string addressInfo)
+    {
+        Name = name;
+        PhoneNumber = phoneNumber;
+        Email = email;
+        AddressInfo = addressInfo;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Phone Number: {PhoneNumber}");
+        Console.WriteLine($"Email: {Email}");
+        Console.WriteLine($"Address: {AddressInfo}");
+        Console.WriteLine();
     }
 }
